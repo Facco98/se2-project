11a048317c8ff9107071e21ec2c54b340e58f798
federica.belotti <federica.belotti98@gmail.com>
2019-12-04 13:39:39 +0100

    Implementata cancellazione della prenotazione

--- services/reserveRoomService.js
+++ services/reserveRoomService.js
@@ -14,4 +14,34 @@ module.exports.init = (envoirment) => {
 
   });
 
+  //gestire cancellazione prenotazione
+  app.delete('/reservation/:id', async (req, resp) => {
+
+    const reservationID = req.params.id; //id della prenotazione
+    const mail = req.mailFromToken; //recupero la mail dell'utente
+    const user = await dbmanager.getUserFromMail(mail); //restituisce tutto l'utente
+    const userId = user.id; //recupero l'id dell'utente
+
+    //controllo se prenotazione esiste
+    const reservation = await dbmanager.getReservation(reservationID);
+
+    if(reservation != false) //se c'è -> controllo se utente è giusto
+    {
+      if(reservation.id_utente == userId || (await dbmanager.checkStaff(mail)) != false) //controllo l'utente
+      {
+        await dbmanager.removeReservation(reservationID); //elimino prenotazione
+        resp.status(200).json({valid: true}); //invio risposta
+      }
+      else //se non è l'utrnte
+      {
+        resp.status(401).json({valid: false, error: 'Unauthorized access'}); //rispondo
+      }
+    }
+    else
+    {
+      resp.status(404).json({valid: false, error: 'Reservation id not valid'});
+    }
+
+  });
+
 };
